cd436c783f096d3a0c69d7e581b5c8e5b6094a5e
jbyuki <jyburkhard@hotmail.com>
2023-03-28 20:48:37 +0200

    clena up old code

--- lua/nabla.lua
+++ lua/nabla.lua
@@ -7,8 +7,6 @@ local ascii = require("nabla.ascii")
 local ts_utils = vim.treesitter
 local utils=require"nabla.utils"
 
-local vtext = vim.api.nvim_create_namespace("nabla")
-
 local autogen_autocmd = {}
 local autogen_flag = false
 
@@ -21,8 +19,6 @@ local mult_virt_ns = {}
 
 local saved_wrapsettings = {}
 
-
-local remove_extmark
 
 local colorize
 
@@ -41,10 +37,6 @@ local disable_virt
 local toggle_virt
 
 local is_virt_enabled
-
-function remove_extmark(events, ns_id)
-  vim.api.nvim_command("autocmd "..table.concat(events, ',').." <buffer> ++once lua pcall(vim.api.nvim_buf_clear_namespace, 0, "..ns_id..", 0, -1)")
-end
 
 function colorize(g, first_dx, dx, dy, ns_id, drawing, px, py, buf)
   if g.t == "num" then
@@ -324,6 +316,7 @@ local function gen_drawing(lines)
     end
 
 
+
     return drawing
   end
   return 0
@@ -379,6 +372,7 @@ local function popup(overrides)
 
 
 
+
     local floating_default_options = {
       wrap = false,
       focusable = false,
@@ -408,8 +402,6 @@ function enable_virt(opts)
 	  vim.api.nvim_buf_clear_namespace(buf, mult_virt_ns[buf], 0, -1)
 	end
 	mult_virt_ns[buf] = vim.api.nvim_create_namespace("")
-
-  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
 
 	local formula_nodes = utils.get_all_mathzones()
 	local formulas_loc = {}
@@ -453,6 +445,7 @@ function enable_virt(opts)
   					drawing[i] = whitespace .. drawing[i]
   				end
   			end
+
 
 
   			local drawing_virt = {}
@@ -715,249 +708,7 @@ end
 
 
 
-local function init()
-	local scratch = vim.api.nvim_create_buf(false, true)
-
-
-	local curbuf = vim.api.nvim_get_current_buf()
-	local attach = vim.api.nvim_buf_attach(curbuf, true, {
-		on_lines = function(_, buf, _, _, _, _, _)
-			vim.schedule(function()
-				vim.api.nvim_buf_set_lines(scratch, 0, -1, true, {})
-
-				vim.api.nvim_buf_clear_namespace( buf, vtext, 0, -1)
-
-				local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
-
-				for y, line in ipairs(lines) do
-					if line ~= "" then
-						local success, exp = pcall(parser.parse_all, line)
-
-						if success and exp then
-							local succ, g = pcall(ascii.to_ascii, {exp}, 1)
-							if not succ then
-							  print(g)
-							  return 0
-							end
-
-							if not g or g == "" then
-							  vim.api.nvim_echo({{"Empty expression detected. Please use the $...$ syntax.", "ErrorMsg"}}, false, {})
-							  return 0
-							end
-
-							local drawing = {}
-							for row in vim.gsplit(tostring(g), "\n") do
-								table.insert(drawing, row)
-							end
-							if whitespace then
-								for i=1,#drawing do
-									drawing[i] = whitespace .. drawing[i]
-								end
-							end
-
-
-							vim.api.nvim_buf_set_lines(scratch, -1, -1, true, drawing)
-
-						else
-							vim.api.nvim_buf_set_virtual_text(buf, vtext, y-1, {{ vim.inspect(errmsg), "Special" }}, {})
-
-						end
-					end
-				end
-
-			end)
-		end
-	})
-
-
-	local prewin = vim.api.nvim_get_current_win()
-	local height = vim.api.nvim_win_get_height(prewin)
-	local width = vim.api.nvim_win_get_width(prewin)
-
-	if width > height*2 then
-		vim.api.nvim_command("vsp")
-	else
-		vim.api.nvim_command("sp")
-	end
-	vim.api.nvim_win_set_buf(prewin, scratch)
-
-end
-
-local function replace_current()
-  local line
-	line = vim.api.nvim_get_current_line()
-
-	local whitespace = string.match(line, "^(%s*)%S")
-
-	local success, exp = pcall(parser.parse_all, line)
-
-	if success and exp then
-		local succ, g = pcall(ascii.to_ascii, {exp}, 1)
-		if not succ then
-		  print(g)
-		  return 0
-		end
-
-		if not g or g == "" then
-		  vim.api.nvim_echo({{"Empty expression detected. Please use the $...$ syntax.", "ErrorMsg"}}, false, {})
-		  return 0
-		end
-
-		local drawing = {}
-		for row in vim.gsplit(tostring(g), "\n") do
-			table.insert(drawing, row)
-		end
-		if whitespace then
-			for i=1,#drawing do
-				drawing[i] = whitespace .. drawing[i]
-			end
-		end
-
-
-		local curline, _ = unpack(vim.api.nvim_win_get_cursor(0))
-		vim.api.nvim_buf_set_lines(0, curline-1, curline, true, drawing)
-
-	else
-		if type(errmsg) == "string"  then
-			print("nabla error: " .. errmsg)
-		else
-			print("nabla error!")
-		end
-	end
-end
-
-local function replace_all()
-	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-
-	result = {}
-	for i,line in ipairs(lines) do
-		if line == "" then
-			table.insert(result, "")
-
-		else
-			local whitespace = string.match(line, "^(%s*)%S")
-
-			local success, exp = pcall(parser.parse_all, line)
-
-			if success and exp then
-				local succ, g = pcall(ascii.to_ascii, {exp}, 1)
-				if not succ then
-				  print(g)
-				  return 0
-				end
-
-				if not g or g == "" then
-				  vim.api.nvim_echo({{"Empty expression detected. Please use the $...$ syntax.", "ErrorMsg"}}, false, {})
-				  return 0
-				end
-
-				local drawing = {}
-				for row in vim.gsplit(tostring(g), "\n") do
-					table.insert(drawing, row)
-				end
-				if whitespace then
-					for i=1,#drawing do
-						drawing[i] = whitespace .. drawing[i]
-					end
-				end
-
-
-				for _, newline in ipairs(drawing) do
-					table.insert(result, newline)
-				end
-
-			else
-				if type(errmsg) == "string"  then
-					print("nabla error: " .. errmsg)
-				else
-					print("nabla error!")
-				end
-			end
-		end
-	end
-	vim.api.nvim_buf_set_lines(0, 0, -1, true, result)
-
-end
-
-local function draw_overlay()
-  local line
-	line = vim.api.nvim_get_current_line()
-
-	local success, exp = pcall(parser.parse_all, line)
-
-	if success and exp then
-		local succ, g = pcall(ascii.to_ascii, {exp}, 1)
-		if not succ then
-		  print(g)
-		  return 0
-		end
-
-		if not g or g == "" then
-		  vim.api.nvim_echo({{"Empty expression detected. Please use the $...$ syntax.", "ErrorMsg"}}, false, {})
-		  return 0
-		end
-
-		local drawing = {}
-		for row in vim.gsplit(tostring(g), "\n") do
-			table.insert(drawing, row)
-		end
-		if whitespace then
-			for i=1,#drawing do
-				drawing[i] = whitespace .. drawing[i]
-			end
-		end
-
-
-		local curline, _ = unpack(vim.api.nvim_win_get_cursor(0))
-		local num_lines = vim.api.nvim_buf_line_count(0)
-		for i=2,#drawing do
-		  local lnum = curline + (i-1)
-		  if lnum > num_lines then
-		    vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
-		  else
-		    local line = vim.api.nvim_buf_get_lines(0, lnum-1, lnum, true)[1]
-		    if line ~= "" then
-		      vim.api.nvim_buf_set_lines(0, lnum-1, lnum-1, true, { "" })
-		    end
-		  end
-		end
-
-    local ns_id = vim.api.nvim_create_namespace("")
-
-    local line = vim.api.nvim_get_current_line()
-    local spaces = ""
-    local len = vim.str_utfindex(line)
-    for i=1,len do
-      spaces = spaces .. " "
-    end
-
-    for i=1,#drawing do
-      vim.api.nvim_buf_set_extmark(0, ns_id, (curline-1)+(i-1), 0, {
-        virt_text = {{drawing[i], "Normal"}, {spaces, "Normal"}},
-        virt_text_pos = "overlay",
-      })
-    end
-
-    remove_extmark({"CursorMoved", "CursorMovedI", "BufHidden", "BufLeave"}, ns_id)
-	else
-		if type(errmsg) == "string"  then
-			print("nabla error: " .. errmsg)
-		else
-			print("nabla error!")
-		end
-	end
-end
-
-
 return {
-	init = init,
-
-	replace_current = replace_current,
-
-	replace_all = replace_all,
-
-	draw_overlay = draw_overlay,
-
 	get_range = get_range,
 
 	gen_drawing = gen_drawing,
@@ -972,3 +723,4 @@ return {
 	is_virt_enabled = is_virt_enabled,
 
 }
+
--- src/core/nabla.lua.t
+++ src/core/nabla.lua.t
@@ -12,73 +12,9 @@
 
 @parse
 
-@init_nabla_mode
-@replace_current_line
-@replace_all_lines
-@place_virt_with_ascii_expression
-
 return {
 	@export_symbols
 }
-@init_nabla_mode+=
-local function init()
-	@create_scratch_buffer
-	@setup_scratch_buffer
-
-	@attach_on_lines_callback
-
-	@create_split_with_scratch_buffer
-end
-
-@export_symbols+=
-init = init,
-
-@create_scratch_buffer+=
-local scratch = vim.api.nvim_create_buf(false, true)
-
-@create_split_with_scratch_buffer+=
-local prewin = vim.api.nvim_get_current_win()
-local height = vim.api.nvim_win_get_height(prewin)
-local width = vim.api.nvim_win_get_width(prewin)
-
-if width > height*2 then
-	vim.api.nvim_command("vsp")
-else
-	vim.api.nvim_command("sp")
-end
-vim.api.nvim_win_set_buf(prewin, scratch)
-
-@attach_on_lines_callback+=
-local curbuf = vim.api.nvim_get_current_buf()
-local attach = vim.api.nvim_buf_attach(curbuf, true, {
-	on_lines = function(_, buf, _, _, _, _, _)
-		vim.schedule(function()
-			@erase_scratch_buffer
-			@clear_all_virtual_text
-			@read_whole_buffer
-			@foreach_nonempty_line_parse_and_output_to_scratch
-		end)
-	end
-})
-
-@erase_scratch_buffer+=
-vim.api.nvim_buf_set_lines(scratch, 0, -1, true, {})
-
-@read_whole_buffer+=
-local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
-
-@foreach_nonempty_line_parse_and_output_to_scratch+=
-for y, line in ipairs(lines) do
-	if line ~= "" then
-		@parse_math_expression
-		if success and exp then
-			@generate_ascii_art
-			@display_in_scratch_buffer
-		else
-			@display_error_as_virtual_text
-		end
-	end
-end
 
 @requires+=
 -- local parser = require("nabla.parser")
@@ -86,15 +22,6 @@ local parser = require("nabla.latex")
 
 @parse_math_expression+=
 local success, exp = pcall(parser.parse_all, line)
-
-@script_variables+=
-local vtext = vim.api.nvim_create_namespace("nabla")
-
-@clear_all_virtual_text+=
-vim.api.nvim_buf_clear_namespace( buf, vtext, 0, -1)
-
-@display_error_as_virtual_text+=
-vim.api.nvim_buf_set_virtual_text(buf, vtext, y-1, {{ vim.inspect(errmsg), "Special" }}, {})
 
 @requires+=
 local ascii = require("nabla.ascii")
@@ -117,36 +44,6 @@ for row in vim.gsplit(tostring(g), "\n") do
 end
 @add_whitespace_to_ascii_art
 
-@display_in_scratch_buffer+=
-vim.api.nvim_buf_set_lines(scratch, -1, -1, true, drawing)
-
-@replace_current_line+=
-local function replace_current()
-  local line
-	@get_current_line
-	@get_whilespace_before
-	@parse_math_expression
-	if success and exp then
-		@generate_ascii_art
-		@replace_current_line_with_ascii_art
-	else
-		if type(errmsg) == "string"  then
-			print("nabla error: " .. errmsg)
-		else
-			print("nabla error!")
-		end
-	end
-end
-
-@export_symbols+=
-replace_current = replace_current,
-
-@get_current_line+=
-line = vim.api.nvim_get_current_line()
-
-@replace_current_line_with_ascii_art+=
-local curline, _ = unpack(vim.api.nvim_win_get_cursor(0))
-vim.api.nvim_buf_set_lines(0, curline-1, curline, true, drawing)
 
 @get_whilespace_before+=
 local whitespace = string.match(line, "^(%s*)%S")
@@ -158,109 +55,3 @@ if whitespace then
 	end
 end
 
-@replace_all_lines+=
-local function replace_all()
-	@get_all_lines
-	result = {}
-	for i,line in ipairs(lines) do
-		if line == "" then
-			@if_empty_line_just_skip
-		else
-			@get_whilespace_before
-			@parse_math_expression
-			if success and exp then
-				@generate_ascii_art
-				@add_generated_to_result
-			else
-				if type(errmsg) == "string"  then
-					print("nabla error: " .. errmsg)
-				else
-					print("nabla error!")
-				end
-			end
-		end
-	end
-	@replace_with_generated
-end
-
-@export_symbols+=
-replace_all = replace_all,
-
-@get_all_lines+=
-local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-
-@replace_with_generated+=
-vim.api.nvim_buf_set_lines(0, 0, -1, true, result)
-
-@if_empty_line_just_skip+=
-table.insert(result, "")
-
-@add_generated_to_result+=
-for _, newline in ipairs(drawing) do
-	table.insert(result, newline)
-end
-
-@place_virt_with_ascii_expression+=
-local function draw_overlay()
-  local line
-	@get_current_line
-	@parse_math_expression
-	if success and exp then
-		@generate_ascii_art
-		@add_some_additionnal_lines_if_not_enough
-    @place_extmarks_with_ascii_art
-    @add_callback_to_clear_extmark_if_cursor_moved
-	else
-		if type(errmsg) == "string"  then
-			print("nabla error: " .. errmsg)
-		else
-			print("nabla error!")
-		end
-	end
-end
-
-@export_symbols+=
-draw_overlay = draw_overlay,
-
-@add_some_additionnal_lines_if_not_enough+=
-local curline, _ = unpack(vim.api.nvim_win_get_cursor(0))
-local num_lines = vim.api.nvim_buf_line_count(0)
-for i=2,#drawing do
-  local lnum = curline + (i-1)
-  if lnum > num_lines then
-    vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
-  else
-    local line = vim.api.nvim_buf_get_lines(0, lnum-1, lnum, true)[1]
-    if line ~= "" then
-      vim.api.nvim_buf_set_lines(0, lnum-1, lnum-1, true, { "" })
-    end
-  end
-end
-
-@place_extmarks_with_ascii_art+=
-local ns_id = vim.api.nvim_create_namespace("")
-
-local line = vim.api.nvim_get_current_line()
-local spaces = ""
-local len = vim.str_utfindex(line)
-for i=1,len do
-  spaces = spaces .. " "
-end
-
-for i=1,#drawing do
-  vim.api.nvim_buf_set_extmark(0, ns_id, (curline-1)+(i-1), 0, {
-    virt_text = {{drawing[i], "Normal"}, {spaces, "Normal"}},
-    virt_text_pos = "overlay",
-  })
-end
-
-@declare_functions+=
-local remove_extmark
-
-@functions+=
-function remove_extmark(events, ns_id)
-  vim.api.nvim_command("autocmd "..table.concat(events, ',').." <buffer> ++once lua pcall(vim.api.nvim_buf_clear_namespace, 0, "..ns_id..", 0, -1)")
-end
-
-@add_callback_to_clear_extmark_if_cursor_moved+=
-remove_extmark({"CursorMoved", "CursorMovedI", "BufHidden", "BufLeave"}, ns_id)
